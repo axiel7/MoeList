73dce440c5c49069ec394bab9fd8dd826ac82016
axiel7 <axiel7lopez@gmail.com>
2020-08-07 18:44:17 +0200

    Login improvements

--- app/src/main/java/com/axiel7/moelist/ui/LoginActivity.kt
+++ app/src/main/java/com/axiel7/moelist/ui/LoginActivity.kt
@@ -1,54 +1,70 @@
 package com.axiel7.moelist.ui
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
-import com.axiel7.moelist.MainActivity
+import androidx.core.content.ContextCompat
 import com.axiel7.moelist.R
 import com.axiel7.moelist.model.AccessToken
 import com.axiel7.moelist.private.ClientId
 import com.axiel7.moelist.rest.LoginService
 import com.axiel7.moelist.rest.ServiceGenerator
 import com.axiel7.moelist.utils.PkceGenerator
+import com.axiel7.moelist.utils.SharedPrefsHelpers
+import com.axiel7.moelist.utils.Urls
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class LoginActivity : Activity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var codeVerifier: String
     private lateinit var codeChallenge: String
+    private lateinit var loadingBar: ProgressBar
 
     companion object {
         const val clientId = ClientId.clientId
-        const val redirectUri = "moelist://moelist.page.link/"
+        const val redirectUri = Urls.redirectUri
         const val state = "MoeList123"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_login)
 
-        window.setDecorFitsSystemWindows(false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+        else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+        }
 
         codeVerifier = PkceGenerator.generateVerifier(128)
         codeChallenge = codeVerifier
 
+        loadingBar = findViewById(R.id.loading_bar)
+
         val loginButton = findViewById<Button>(R.id.login)
         loginButton.setOnClickListener {
             val builder = CustomTabsIntent.Builder()
-            builder.setToolbarColor(resources.getColor(R.color.colorPrimary))
+            builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
             val chromeIntent = builder.build()
             chromeIntent.intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
             chromeIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            chromeIntent.launchUrl(this, Uri.parse("https://myanimelist.net" + "/v1/oauth2/authorize" + "?response_type=code"
-                    + "&client_id=" + clientId + "&code_challenge=" + codeVerifier + "&state=" + state))
+            chromeIntent.launchUrl(this, Uri.parse(Urls.oauthBaseUrl + "authorize" + "?response_type=code"
+                    + "&client_id=" + clientId + "&code_challenge=" + codeVerifier + "&state=" + state
+            ))
         }
     }
 
@@ -57,9 +73,10 @@ class LoginActivity : Activity() {
 
         val uri = intent?.data
         if (uri!=null && uri.toString().startsWith(redirectUri)) {
+            loadingBar.visibility = View.VISIBLE
             val code = uri.getQueryParameter("code")
             val receivedState = uri.getQueryParameter("state")
-            if (code!=null && receivedState==state) {
+            if (code!=null && receivedState== state) {
                 val loginService = ServiceGenerator.createService(LoginService::class.java)
                 val call :Call<AccessToken> = loginService.getAccessToken(clientId, code, codeVerifier,"authorization_code")
                 var accessToken: AccessToken?
@@ -72,13 +89,13 @@ class LoginActivity : Activity() {
                         val token = accessToken?.access_token
                         if (token != null) {
                             Log.d("MoeLog", "AccessToken=$token")
-                            val sharedPref = this@LoginActivity.getSharedPreferences(getString(R.string.shared_preferences) ,Context.MODE_PRIVATE) ?: return
-                            with (sharedPref.edit()) {
-                                putString("accessToken", accessToken?.access_token)
-                                putString("refreshToken", accessToken?.refresh_token)
-                                putBoolean("isUserLogged", true)
-                                apply()
-                            }
+
+                            SharedPrefsHelpers.init(this@LoginActivity)
+                            val sharedPref = SharedPrefsHelpers.instance
+                            sharedPref?.saveString("accessToken", accessToken?.access_token)
+                            sharedPref?.saveString("refreshToken", accessToken?.refresh_token)
+                            sharedPref?.saveBoolean("isUserLogged", true)
+
                             val openMainActivity = Intent(this@LoginActivity, MainActivity::class.java)
                             startActivity(openMainActivity)
                         }
@@ -91,6 +108,7 @@ class LoginActivity : Activity() {
                 })
             }
             else if (uri.getQueryParameter("error")!=null) {
+                loadingBar.visibility = View.INVISIBLE
                 Toast.makeText(this, "Login error", Toast.LENGTH_SHORT).show()
             }
         }
