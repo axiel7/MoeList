90ee7e57474636889619226b9ad04aa2f0895ca9
axiel7 <axiel7lopez@gmail.com>
2020-08-07 18:38:11 +0200

    Refactoring

---
+++ app/src/main/java/com/axiel7/moelist/adapter/ListAnimeAdapter.kt
@@ -0,0 +1,61 @@
+package com.axiel7.moelist.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import com.axiel7.moelist.R
+import com.axiel7.moelist.model.AnimeList
+
+class ListAnimeAdapter(private val animes: MutableList<AnimeList>,
+                       private val rowLayout: Int,
+                       private val context: Context,
+                       private val onClickListener: (View, AnimeList) -> Unit) :
+    RecyclerView.Adapter<ListAnimeAdapter.AnimeViewHolder>() {
+    private var endListReachedListener: EndListReachedListener? = null
+
+    inner class AnimeViewHolder internal constructor(v: View) : RecyclerView.ViewHolder(v) {
+        val animeTitle: TextView = v.findViewById(R.id.anime_title)
+        val animePoster: ImageView = v.findViewById(R.id.anime_poster)
+
+        init {
+            animePoster.clipToOutline = true
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(rowLayout, parent, false)
+        return AnimeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AnimeViewHolder, position: Int) {
+        val posterUrl = animes[position].node?.main_picture?.medium
+        val animeTitle = animes[position].node?.title
+        holder.animePoster.load(posterUrl) {
+            crossfade(true)
+            crossfade(500)
+            error(R.drawable.ic_launcher_foreground)
+            allowHardware(false)
+        }
+        holder.animeTitle.text = animeTitle
+        val anime = animes[position]
+        holder.itemView.setOnClickListener { view ->
+            onClickListener(view, anime)
+        }
+        if (position == animes.size - 2) run {
+            endListReachedListener?.onBottomReached(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return animes.size
+    }
+
+    fun setEndListReachedListener(endListReachedListener: EndListReachedListener?) {
+        this.endListReachedListener = endListReachedListener
+    }
+}
