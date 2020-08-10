a8391a932b71cb3ce7ee24aa28656b767b2aeff0
axiel7 <axiel7lopez@gmail.com>
2020-08-10 18:22:26 +0200

    Small improvements

--- app/src/main/java/com/axiel7/moelist/MyApplication.kt
+++ app/src/main/java/com/axiel7/moelist/MyApplication.kt
@@ -1,12 +1,26 @@
 package com.axiel7.moelist
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.util.CoilUtils
 import com.axiel7.moelist.room.AnimeDatabase
+import okhttp3.OkHttpClient
 
-class MyApplication : Application() {
+class MyApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         animeDb = AnimeDatabase.getAnimeDatabase(applicationContext)
+    }
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(applicationContext)
+            .crossfade(true)
+            .okHttpClient {
+                OkHttpClient.Builder()
+                    .cache(CoilUtils.createDefaultCache(applicationContext))
+                    .build()
+            }
+            .build()
     }
 
     companion object {
