ec8e31d9ed365b4d552e840fcd8362f37764681d
Axel <axiel7lopez@gmail.com>
2021-01-14 09:28:01 +0100

    Optimize splash screen

--- app/src/main/java/com/axiel7/moelist/ui/BaseActivity.kt
+++ app/src/main/java/com/axiel7/moelist/ui/BaseActivity.kt
@@ -7,8 +7,8 @@ import com.axiel7.moelist.R
 
 open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
