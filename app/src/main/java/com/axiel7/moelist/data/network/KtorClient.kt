b8b74d403e2261b67a419d01954cef1e3ec1247a
Axel <axiel7lopez@gmail.com>
2022-08-19 11:26:02 +0200

    Optimize imports

--- app/src/main/java/com/axiel7/moelist/data/network/KtorClient.kt
+++ app/src/main/java/com/axiel7/moelist/data/network/KtorClient.kt
@@ -5,10 +5,10 @@ import com.axiel7.moelist.BuildConfig
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
-import io.ktor.client.request.*
-import io.ktor.http.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import java.util.concurrent.TimeUnit
