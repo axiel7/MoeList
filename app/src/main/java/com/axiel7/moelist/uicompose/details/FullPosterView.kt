0193c519b7271a92b96d29ad825395cde027b28d
Axel <axiel7lopez@gmail.com>
2023-04-25 09:30:37 +0900

    Full poster: open image in browser

--- app/src/main/java/com/axiel7/moelist/uicompose/details/FullPosterView.kt
+++ app/src/main/java/com/axiel7/moelist/uicompose/details/FullPosterView.kt
@@ -15,39 +15,64 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
+import com.axiel7.moelist.R
 import com.axiel7.moelist.uicompose.composables.DefaultTopAppBar
 import com.axiel7.moelist.uicompose.theme.MoeListTheme
+import com.axiel7.moelist.utils.ContextExtensions.openLink
 import kotlinx.coroutines.launch
 
 const val FULL_POSTER_DESTINATION = "full_poster/{pictures}"
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun FullPosterView(
     pictures: Array<String>,
     navController: NavController
 ) {
+    val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
     val pagerState = rememberPagerState()
 
     Scaffold(
         topBar = {
-            DefaultTopAppBar(
-                title = "",
-                navController = navController
+            TopAppBar(
+                title = { },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(painter = painterResource(R.drawable.ic_arrow_back), contentDescription = "back")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        context.openLink(pictures[pagerState.currentPage])
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_open_in_browser),
+                            contentDescription = stringResource(R.string.view_on_mal)
+                        )
+                    }
+                }
             )
         }
     ) {
