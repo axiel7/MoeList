1680aebe1e197d985b2c176a76385a4a8c7dd2aa
Axel <axiel7lopez@gmail.com>
2023-05-09 16:14:38 +0900

    Widget: add click action

--- app/src/main/java/com/axiel7/moelist/uicompose/airingwidget/AiringWidget.kt
+++ app/src/main/java/com/axiel7/moelist/uicompose/airingwidget/AiringWidget.kt
@@ -1,17 +1,21 @@
 package com.axiel7.moelist.uicompose.airingwidget
 
 import android.content.Context
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalContext
 import androidx.glance.action.ActionParameters
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.CircularProgressIndicator
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.action.ActionCallback
 import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.currentState
@@ -22,6 +26,7 @@ import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import com.axiel7.moelist.R
 import com.axiel7.moelist.data.model.anime.nextAiringDayFormatted
+import com.axiel7.moelist.uicompose.MainActivity
 import com.axiel7.moelist.uicompose.theme.AppWidgetBox
 import com.axiel7.moelist.uicompose.theme.AppWidgetColumn
 import com.axiel7.moelist.uicompose.theme.stringResource
@@ -48,7 +53,18 @@ class AiringWidget : GlanceAppWidget() {
                         LazyColumn {
                             items(airingInfo.animeList) { item ->
                                 Column(
-                                    modifier = GlanceModifier.padding(bottom = 8.dp)
+                                    modifier = GlanceModifier
+                                        .padding(bottom = 8.dp)
+                                        .clickable(actionStartActivity(
+                                            Intent(LocalContext.current, MainActivity::class.java).apply {
+                                                action = "details"
+                                                putExtra("media_id", item.id)
+                                                putExtra("media_type", "anime")
+                                                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                                addCategory(item.id.toString())
+                                            }
+                                        ))
                                 ) {
                                     Text(
                                         text = item.title,
