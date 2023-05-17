cb9448d82eb21eb18a8946ca65f5d665125d65ef
Axel <axiel7lopez@gmail.com>
2023-05-17 12:09:15 +0900

    Change widget implementation to glance 1.0.0-beta01

--- app/src/main/java/com/axiel7/moelist/uicompose/airingwidget/AiringWidget.kt
+++ app/src/main/java/com/axiel7/moelist/uicompose/airingwidget/AiringWidget.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
+import androidx.glance.ButtonDefaults
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.LocalContext
 import androidx.glance.action.ActionParameters
 import androidx.glance.action.clickable
-import androidx.glance.appwidget.CircularProgressIndicator
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.action.ActionCallback
@@ -19,117 +19,130 @@ import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
-import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
+import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import com.axiel7.moelist.R
+import com.axiel7.moelist.data.datastore.PreferencesDataStore
+import com.axiel7.moelist.data.datastore.PreferencesDataStore.defaultPreferencesDataStore
+import com.axiel7.moelist.data.datastore.PreferencesDataStore.getValueSync
+import com.axiel7.moelist.data.model.anime.AnimeNode
 import com.axiel7.moelist.data.model.anime.nextAiringDayFormatted
+import com.axiel7.moelist.data.repository.AnimeRepository
 import com.axiel7.moelist.uicompose.MainActivity
-import com.axiel7.moelist.uicompose.theme.AppWidgetBox
 import com.axiel7.moelist.uicompose.theme.AppWidgetColumn
 import com.axiel7.moelist.uicompose.theme.stringResource
 
 class AiringWidget : GlanceAppWidget() {
 
-    override val stateDefinition = AiringInfoStateDefinition
+    override val stateDefinition = PreferencesGlanceStateDefinition
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-        AiringWidgetWorker.enqueue(context)
+        val animeList = getAiringAnime(context)
 
         provideContent {
-            val airingInfo = currentState<AiringInfo>()
-
             GlanceTheme {
-                when (airingInfo) {
-                    is AiringInfo.Loading -> {
-                        AppWidgetBox(contentAlignment = Alignment.Center) {
-                            CircularProgressIndicator(color = GlanceTheme.colors.primary)
-                        }
-                    }
-                    is AiringInfo.Available -> {
-                        if (airingInfo.animeList.isEmpty()) {
-                            AppWidgetBox(contentAlignment = Alignment.Center) {
-                                Text(text = stringResource(R.string.nothing_today))
-                            }
-                        }
-                        else AppWidgetColumn {
-                            LazyColumn {
-                                items(airingInfo.animeList) { item ->
-                                    Column(
-                                        modifier = GlanceModifier
-                                            .padding(bottom = 8.dp)
-                                            .fillMaxWidth()
-                                            .clickable(actionStartActivity(
-                                                Intent(LocalContext.current, MainActivity::class.java).apply {
-                                                    action = "details"
-                                                    putExtra("media_id", item.id)
-                                                    putExtra("media_type", "anime")
-                                                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                                                    addCategory(item.id.toString())
-                                                }
-                                            ))
-                                    ) {
-                                        Text(
-                                            text = item.title,
-                                            style = TextStyle(
-                                                color = GlanceTheme.colors.onSurfaceVariant
-                                            ),
-                                            maxLines = 1
-                                        )
-                                        Text(
-                                            text = item.broadcast!!.nextAiringDayFormatted()
-                                                ?: stringResource(R.string.unknown),
-                                            style = TextStyle(
-                                                color = GlanceTheme.colors.onPrimaryContainer
-                                            ),
-                                            maxLines = 1
-                                        )
-                                    }
+                if (animeList.isNullOrEmpty()) {
+                    AppWidgetColumn(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = stringResource(R.string.nothing_today),
+                            modifier = GlanceModifier.padding(bottom = 8.dp),
+                            style = TextStyle(
+                                color = GlanceTheme.colors.onSurface
+                            )
+                        )
+                        Button(
+                            text = stringResource(R.string.refresh),
+                            onClick = actionRunCallback<RefreshAiringInfoAction>(),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = GlanceTheme.colors.primaryContainer,
+                                contentColor = GlanceTheme.colors.onPrimaryContainer
+                            )
+                        )
+                    }//: Column
+                } else {
+                    AppWidgetColumn {
+                        LazyColumn {
+                            items(animeList) { item ->
+                                Column(
+                                    modifier = GlanceModifier
+                                        .padding(bottom = 8.dp)
+                                        .fillMaxWidth()
+                                        .clickable(actionStartActivity(
+                                            Intent(LocalContext.current, MainActivity::class.java).apply {
+                                                action = "details"
+                                                putExtra("media_id", item.id)
+                                                putExtra("media_type", "anime")
+                                                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                                addCategory(item.id.toString())
+                                            }
+                                        ))
+                                ) {
+                                    Text(
+                                        text = item.title,
+                                        style = TextStyle(
+                                            color = GlanceTheme.colors.onSurfaceVariant
+                                        ),
+                                        maxLines = 1
+                                    )
+                                    Text(
+                                        text = item.broadcast!!.nextAiringDayFormatted()
+                                            ?: stringResource(R.string.unknown),
+                                        style = TextStyle(
+                                            color = GlanceTheme.colors.onPrimaryContainer
+                                        ),
+                                        maxLines = 1
+                                    )
                                 }
                             }
-                        }
-                    }
-                    is AiringInfo.Unavailable -> {
-                        AppWidgetColumn(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(
-                                text = airingInfo.message,
-                                modifier = GlanceModifier.padding(bottom = 8.dp)
-                            )
-                            Button(
-                                text = stringResource(R.string.refresh),
-                                onClick = actionRunCallback<UpdateAiringInfoAction>()
-                            )
-                        }
-                    }
+                        }//: LazyColumn
+                    }//: Column
                 }
             }
         }
     }
+}
 
-    override suspend fun onDelete(context: Context, glanceId: GlanceId) {
-        super.onDelete(context, glanceId)
-        AiringWidgetWorker.cancel(context)
+suspend fun getAiringAnime(context: Context): List<AnimeNode>? {
+    return try {
+        val token = context.defaultPreferencesDataStore
+            .getValueSync(PreferencesDataStore.ACCESS_TOKEN_PREFERENCE_KEY)!!
+        val nsfw = context.defaultPreferencesDataStore
+            .getValueSync(PreferencesDataStore.NSFW_PREFERENCE_KEY) ?: false
+        AnimeRepository.getAiringAnimeOnList(token = token, nsfw = nsfw)!!
+    } catch (e: Exception) {
+        null
     }
 }
 
-class UpdateAiringInfoAction : ActionCallback {
+class RefreshAiringInfoAction : ActionCallback {
     override suspend fun onAction(
         context: Context,
         glanceId: GlanceId,
         parameters: ActionParameters
     ) {
-        AiringWidgetWorker.enqueue(context = context, force = true)
+        getAiringAnime(context)
     }
 }
 
 class AiringWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = AiringWidget()
+
+    override fun onEnabled(context: Context) {
+        super.onEnabled(context)
+        AiringWidgetWorker.enqueue(context)
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        super.onDeleted(context, appWidgetIds)
+        AiringWidgetWorker.cancel(context)
+    }
 }
--- app/src/main/java/com/axiel7/moelist/uicompose/airingwidget/AiringWidgetWorker.kt
+++ app/src/main/java/com/axiel7/moelist/uicompose/airingwidget/AiringWidgetWorker.kt
@@ -1,22 +1,12 @@
 package com.axiel7.moelist.uicompose.airingwidget
 
 import android.content.Context
-import androidx.glance.GlanceId
-import androidx.glance.appwidget.GlanceAppWidgetManager
-import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.appwidget.updateAll
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
-import com.axiel7.moelist.data.datastore.PreferencesDataStore.ACCESS_TOKEN_PREFERENCE_KEY
-import com.axiel7.moelist.data.datastore.PreferencesDataStore.NSFW_PREFERENCE_KEY
-import com.axiel7.moelist.data.datastore.PreferencesDataStore.defaultPreferencesDataStore
-import com.axiel7.moelist.data.datastore.PreferencesDataStore.getValueSync
-import com.axiel7.moelist.data.repository.AnimeRepository
 import java.time.Duration
 
 class AiringWidgetWorker(
@@ -25,73 +15,26 @@ class AiringWidgetWorker(
 ) : CoroutineWorker(context, workerParameters) {
 
     override suspend fun doWork(): Result {
-        val manager = GlanceAppWidgetManager(context)
-        val glanceIds = manager.getGlanceIds(AiringWidget::class.java)
-        return try {
-            // Update state to indicate loading
-            setWidgetState(glanceIds, AiringInfo.Loading)
+        if (isStopped) return Result.success()
 
-            // Update state with new data
-            val token = context.defaultPreferencesDataStore.getValueSync(ACCESS_TOKEN_PREFERENCE_KEY)!!
-            val nsfw = context.defaultPreferencesDataStore.getValueSync(NSFW_PREFERENCE_KEY) ?: false
-            setWidgetState(glanceIds, AiringInfo.Available(
-                animeList = AnimeRepository.getAiringAnimeOnList(token = token, nsfw = nsfw)!!
-            ))
-
-            Result.success()
-        } catch (e: Exception) {
-            setWidgetState(glanceIds, AiringInfo.Unavailable(e.message.orEmpty()))
-            if (runAttemptCount < 10) {
-                // Exponential backoff strategy will avoid the request to repeat
-                // too fast in case of failures.
-                Result.retry()
-            } else {
-                Result.failure()
-            }
-        }
-    }
-
-    private suspend fun setWidgetState(glanceIds: List<GlanceId>, newState: AiringInfo) {
-        glanceIds.forEach { glanceId ->
-            updateAppWidgetState(
-                context = context,
-                definition = AiringInfoStateDefinition,
-                glanceId = glanceId,
-                updateState = { newState }
-            )
-        }
         AiringWidget().updateAll(context)
+        return Result.success()
     }
 
     companion object {
         private const val uniqueWorkName = "AiringWidgetWorker"
 
-        fun enqueue(context: Context, force: Boolean = false) {
+        fun enqueue(context: Context) {
             val manager = WorkManager.getInstance(context)
             val requestBuilder = PeriodicWorkRequestBuilder<AiringWidgetWorker>(
                 Duration.ofHours(12)
             )
-            var workPolicy = ExistingPeriodicWorkPolicy.KEEP
-
-            // Replace any enqueued work and expedite the request
-            if (force) {
-                workPolicy = ExistingPeriodicWorkPolicy.UPDATE
-            }
+            val workPolicy = ExistingPeriodicWorkPolicy.UPDATE
 
             manager.enqueueUniquePeriodicWork(
                 uniqueWorkName,
                 workPolicy,
                 requestBuilder.build()
-            )
-
-            // Temporary workaround to avoid WM provider to disable itself and trigger an
-            // app widget update
-            manager.enqueueUniqueWork(
-                "$uniqueWorkName-workaround",
-                ExistingWorkPolicy.KEEP,
-                OneTimeWorkRequestBuilder<AiringWidgetWorker>().apply {
-                    setInitialDelay(Duration.ofDays(365))
-                }.build()
             )
         }
 
