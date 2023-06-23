0fd2eb32a258ee45bffceaa231ab8bf9da75c248
Axel <axiel7lopez@gmail.com>
2023-06-23 19:01:28 +0900

    Fix #71

--- app/src/main/java/com/axiel7/moelist/uicompose/composables/SelectableIconToggleButton.kt
+++ app/src/main/java/com/axiel7/moelist/uicompose/composables/SelectableIconToggleButton.kt
@@ -6,9 +6,12 @@ import androidx.compose.material3.FilledIconToggleButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.PlainTooltipBox
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberPlainTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,12 +22,20 @@ fun <T> SelectableIconToggleButton(
     selectedValue: T,
     onClick: (Boolean) -> Unit
 ) {
+    val tooltipState = rememberPlainTooltipState()
+    val scope = rememberCoroutineScope()
+
     PlainTooltipBox(
         tooltip = { Text(tooltipText) },
+        focusable = false,
+        tooltipState = tooltipState
     ) {
         FilledIconToggleButton(
             checked = value == selectedValue,
-            onCheckedChange = onClick,
+            onCheckedChange = {
+                scope.launch { tooltipState.show() }
+                onClick(it)
+            },
             modifier = Modifier.tooltipTrigger()
         ) {
             Icon(painter = painterResource(icon), contentDescription = tooltipText)
