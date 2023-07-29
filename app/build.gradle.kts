78dc119255c0a758f7c8c68802b9d1ffb13c1ea5
axiel7 <axiel7lopez@gmail.com>
2023-07-29 10:59:48 +0900

    update glance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.0-beta02")
 
-    implementation("androidx.glance:glance-appwidget:1.0.0-beta01")
+    implementation("androidx.glance:glance-appwidget:1.0.0-rc01")
 
     val accompanistVersion = "0.30.1"
     implementation("com.google.accompanist:accompanist-insets-ui:$accompanistVersion")
