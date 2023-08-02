e1faeeed86e3c6d3fa296fccf7e5df10b3ddb945
axiel7 <axiel7lopez@gmail.com>
2023-08-02 16:28:50 +0900

    upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "8.1.0-rc01"
+    val agpVersion = "8.1.0"
     val kotlinVersion = "1.8.21"
     id("com.android.application").version(agpVersion).apply(false)
     id("com.android.library").version(agpVersion).apply(false)
