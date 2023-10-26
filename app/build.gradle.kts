fd5f7e237512ae232f0ebea20308b9014000adf9
axiel7 <axiel7lopez@gmail.com>
2023-10-26 14:10:56 +0900

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,15 +99,15 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     //Compose
-    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
-    implementation("androidx.compose.material3:material3-android:1.2.0-alpha08")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha09")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha08")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha09")
 
-    implementation("androidx.activity:activity-compose:1.8.0-rc01")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
     implementation("androidx.glance:glance-appwidget:1.0.0")
 
