34e78c853b2fc903df29a4082536170fa3d61e1c
axiel7 <axiel7lopez@gmail.com>
2023-11-22 14:26:53 +0900

    upgrade kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ android {
         jvmTarget = "1.8"
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val agpVersion = "8.1.4"
-    val kotlinVersion = "1.9.10"
+    val kotlinVersion = "1.9.20"
     id("com.android.application").version(agpVersion).apply(false)
     id("com.android.library").version(agpVersion).apply(false)
     id("org.jetbrains.kotlin.android").version(kotlinVersion).apply(false)
