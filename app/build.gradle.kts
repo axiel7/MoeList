dacdc7f81edb24094661e16c98728a1c742233eb
axiel7 <axiel7lopez@gmail.com>
2023-12-01 11:00:48 +0900

    update material3 1.2.0-alpha12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,10 +102,11 @@ dependencies {
 
     //Compose
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    implementation("androidx.compose.material3:material3-android:1.2.0-alpha11")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha11")
+    val materialVersion = "1.2.0-alpha12"
+    implementation("androidx.compose.material3:material3-android:$materialVersion")
+    implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
     implementation("androidx.activity:activity-compose:1.8.1")
 
