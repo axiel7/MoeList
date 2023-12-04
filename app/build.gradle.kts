3bdb6742e2fef6293b90e535617b0ff4333d72c1
axiel7 <axiel7lopez@gmail.com>
2023-12-04 16:27:11 +0900

    update kotlin serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,7 +136,7 @@ dependencies {
     implementation("io.ktor:ktor-client-auth:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     //Utils
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
