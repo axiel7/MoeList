de538e2725f228aab288f6b6e8fc7b94cd6af276
axiel7 <axiel7lopez@gmail.com>
2023-12-15 17:29:56 +0900

    update kotlin 1.9.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ android {
         jvmTarget = "1.8"
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.5"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
