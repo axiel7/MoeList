b1fec062a234b908f6ea92c0bd8afc1e28fa09e6
axiel7 <axiel7lopez@gmail.com>
2024-01-04 09:26:25 +0900

    ktor: use default timeout and logger

--- app/src/main/java/com/axiel7/moelist/data/network/KtorClient.kt
+++ app/src/main/java/com/axiel7/moelist/data/network/KtorClient.kt
@@ -1,14 +1,13 @@
 package com.axiel7.moelist.data.network
 
-import android.util.Log
 import com.axiel7.moelist.App
 import com.axiel7.moelist.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -16,45 +15,26 @@ import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import java.util.concurrent.TimeUnit
 
 val ktorHttpClient = HttpClient(OkHttp) {
 
     expectSuccess = false
 
     install(ContentNegotiation) {
-        json(Json {
-            coerceInputValues = true
-            isLenient = true
-            ignoreUnknownKeys = true
-        })
+        json(
+            Json {
+                coerceInputValues = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            }
+        )
     }
 
     install(HttpCache)
 
-    engine {
-        config {
-            connectTimeout(Int.MAX_VALUE.toLong(), TimeUnit.MILLISECONDS)
-            callTimeout(Int.MAX_VALUE.toLong(), TimeUnit.MILLISECONDS)
-            readTimeout(Int.MAX_VALUE.toLong(), TimeUnit.MILLISECONDS)
-            writeTimeout(Int.MAX_VALUE.toLong(), TimeUnit.MILLISECONDS)
-            retryOnConnectionFailure(true)
-        }
-    }
-
-    install(HttpTimeout) {
-        socketTimeoutMillis = Int.MAX_VALUE.toLong()
-        connectTimeoutMillis = Int.MAX_VALUE.toLong()
-        requestTimeoutMillis = Int.MAX_VALUE.toLong()
-    }
-
     if (BuildConfig.IS_DEBUG) {
         install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    Log.v("Logger Ktor =>", message)
-                }
-            }
+            logger = Logger.ANDROID
             level = LogLevel.ALL
         }
     }
