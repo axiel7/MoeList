50eb0964e1565bd41d8e01d3dfc345368d2daf47
axiel7 <axiel7lopez@gmail.com>
2024-05-02 10:04:40 +0200

    update datastore 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
 
     implementation("androidx.browser:browser:1.8.0")
-    implementation("androidx.datastore:datastore-preferences:1.1.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.work:work-runtime:2.9.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
