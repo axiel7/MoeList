6446847ee5974331dc5ea81d155b3d49db11f99b
axiel7 <axiel7lopez@gmail.com>
2024-05-03 09:03:28 +0200

    notifications: continue after granted permission

--- app/src/main/java/com/axiel7/moelist/ui/details/composables/MediaDetailsTopAppBar.kt
+++ app/src/main/java/com/axiel7/moelist/ui/details/composables/MediaDetailsTopAppBar.kt
@@ -5,14 +5,11 @@ import android.os.Build
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import com.axiel7.moelist.R
 import com.axiel7.moelist.data.model.anime.AnimeDetails
 import com.axiel7.moelist.data.model.media.MediaStatus
@@ -27,7 +24,6 @@ import com.axiel7.moelist.utils.DateUtils.parseDate
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import kotlinx.coroutines.launch
 import java.time.LocalTime
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
@@ -39,17 +35,59 @@ fun MediaDetailsTopAppBar(
     navigateBack: () -> Unit
 ) {
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
     val savedForNotification = when (uiState.mediaDetails?.status) {
         MediaStatus.AIRING -> uiState.notification
-
         MediaStatus.NOT_AIRED -> uiState.startNotification
-
         else -> null
     }
+
+    fun onClickNotification(permissionGranted: Boolean) {
+        val enable = savedForNotification == null
+        (uiState.mediaDetails as? AnimeDetails)?.let { details ->
+            if (enable && permissionGranted) {
+                if (details.status != MediaStatus.NOT_AIRED
+                    && details.broadcast?.dayOfTheWeek != null
+                    && details.broadcast.startTime != null
+                ) {
+                    event?.scheduleAiringAnimeNotification(
+                        title = details.title.orEmpty(),
+                        animeId = details.id,
+                        weekDay = details.broadcast.dayOfTheWeek,
+                        jpHour = LocalTime.parse(details.broadcast.startTime)
+                    )
+                    context.showToast(R.string.airing_notification_enabled)
+                } else if (details.status == MediaStatus.NOT_AIRED && details.startDate != null) {
+                    val startDate = details.startDate.parseDate()
+                    if (startDate != null) {
+                        event?.scheduleAnimeStartNotification(
+                            title = details.title.orEmpty(),
+                            animeId = details.id,
+                            startDate = startDate
+                        )
+                        context.showToast(R.string.start_airing_notification_enabled)
+                    } else {
+                        context.showToast(R.string.invalid_start_date)
+                    }
+                } else {
+                    if (details.broadcast?.dayOfTheWeek == null
+                        || details.broadcast.startTime == null
+                    ) {
+                        context.showToast(R.string.invalid_broadcast)
+                    } else if (details.startDate == null) {
+                        context.showToast(R.string.invalid_start_date)
+                    }
+                }
+            } else {
+                event?.removeAiringAnimeNotification(animeId = details.id)
+                context.showToast("Notification disabled")
+            }
+        }
+    }
+
     val notificationPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         rememberPermissionState(
-            Manifest.permission.POST_NOTIFICATIONS
+            permission = Manifest.permission.POST_NOTIFICATIONS,
+            onPermissionResult = { onClickNotification(it) }
         )
     } else null
 
@@ -64,57 +102,10 @@ fun MediaDetailsTopAppBar(
             ) {
                 IconButton(
                     onClick = {
-                        val enable = savedForNotification == null
-                        (uiState.mediaDetails as? AnimeDetails)?.let { details ->
-                            if (enable) {
-                                if (notificationPermission == null
-                                    || notificationPermission.status.isGranted
-                                ) {
-                                    scope.launch {
-                                        if (details.status != MediaStatus.NOT_AIRED
-                                            && details.broadcast?.dayOfTheWeek != null
-                                            && details.broadcast.startTime != null
-                                        ) {
-                                            event?.scheduleAiringAnimeNotification(
-                                                title = details.title.orEmpty(),
-                                                animeId = details.id,
-                                                weekDay = details.broadcast.dayOfTheWeek,
-                                                jpHour = LocalTime.parse(details.broadcast.startTime)
-                                            )
-                                            context.showToast(R.string.airing_notification_enabled)
-                                        } else if (details.status == MediaStatus.NOT_AIRED
-                                            && details.startDate != null
-                                        ) {
-                                            val startDate = details.startDate.parseDate()
-                                            if (startDate != null) {
-                                                event?.scheduleAnimeStartNotification(
-                                                    title = details.title.orEmpty(),
-                                                    animeId = details.id,
-                                                    startDate = startDate
-                                                )
-                                                context.showToast(R.string.start_airing_notification_enabled)
-                                            } else {
-                                                context.showToast(R.string.invalid_start_date)
-                                            }
-                                        } else {
-                                            if (details.broadcast?.dayOfTheWeek == null
-                                                || details.broadcast.startTime == null
-                                            ) {
-                                                context.showToast(R.string.invalid_broadcast)
-                                            } else if (details.startDate == null) {
-                                                context.showToast(R.string.invalid_start_date)
-                                            }
-                                        }
-                                    }
-                                } else {
-                                    notificationPermission.launchPermissionRequest()
-                                }
-                            } else {
-                                scope.launch {
-                                    event?.removeAiringAnimeNotification(animeId = details.id)
-                                    context.showToast("Notification disabled")
-                                }
-                            }
+                        if (notificationPermission == null || notificationPermission.status.isGranted) {
+                            onClickNotification(true)
+                        } else {
+                            notificationPermission.launchPermissionRequest()
                         }
                     }
                 ) {
