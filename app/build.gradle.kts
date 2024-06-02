ea2c6af4f1538aa8727b27a45d00b86f36b6d6aa
axiel7 <axiel7lopez@gmail.com>
2024-06-02 09:45:04 +0200

    chore(deps): lifecycle 2.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,7 +126,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.8.0-beta02")
 
-    val lifecycleVersion = "2.8.0"
+    val lifecycleVersion = "2.8.1"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
 
