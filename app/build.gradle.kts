05655244b5740638a4f8474d4cc8f8a3fd1d7172
axiel7 <axiel7lopez@gmail.com>
2024-06-14 12:36:27 +0200

    chore(deps): update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,23 +114,23 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     //Compose
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    val materialVersion = "1.3.0-beta02"
+    val materialVersion = "1.3.0-beta03"
     implementation("androidx.compose.material3:material3-android:$materialVersion")
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta02")
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta03")
 
-    val lifecycleVersion = "2.8.1"
+    val lifecycleVersion = "2.8.2"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
 
-    implementation("androidx.glance:glance-appwidget:1.0.0")
+    implementation("androidx.glance:glance-appwidget:1.1.0")
 
     val accompanistVersion = "0.34.0"
     implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
