77a3839706ef96767836f40d57212ccfa5e67ac9
axiel7 <axiel7lopez@gmail.com>
2024-06-28 08:37:18 +0200

    fix(deps): material3 1.3.0-beta04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,7 +107,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    val materialVersion = "1.3.0-beta03"
+    val materialVersion = "1.3.0-beta04"
     implementation("androidx.compose.material3:material3-android:$materialVersion")
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
