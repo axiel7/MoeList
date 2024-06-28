9efe984c56dd51afb2c9367538cfb39e8d07c0bd
axiel7 <axiel7lopez@gmail.com>
2024-06-28 08:38:07 +0200

    fix(deps): android tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -127,8 +127,8 @@ dependencies {
 
     //Test
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     //Ktor
     val ktorVersion = "2.3.12"
