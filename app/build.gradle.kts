fa5d7b5f90e04ae0f5958526ceeaed0488bcdeb9
axiel7 <axiel7lopez@gmail.com>
2024-06-28 08:37:32 +0200

    fix(deps): navigation 2.8.0-beta04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta03")
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta04")
 
     val lifecycleVersion = "2.8.2"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
