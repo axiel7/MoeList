3d96207762341244456d713afa09e1ef83c77b7a
axiel7 <axiel7lopez@gmail.com>
2024-07-05 10:03:49 +0200

    fix(deps): lifecycle 2.8.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.8.0-beta04")
 
-    val lifecycleVersion = "2.8.2"
+    val lifecycleVersion = "2.8.3"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
 
