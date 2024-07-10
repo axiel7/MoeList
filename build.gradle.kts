9731f8959d1d9e34f4de85977f3f4eed6bf36c81
axiel7 <axiel7lopez@gmail.com>
2024-07-10 22:03:17 +0200

    fix(deps): ksp 1.0.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
-    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.21" apply false
+    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.22" apply false
 }
