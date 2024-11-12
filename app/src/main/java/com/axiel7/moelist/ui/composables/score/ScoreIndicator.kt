e737792c6bb7546765e2a4a729eaccd45172a718
blackholeearth <blackholeearth@users.noreply.github.com>
2024-11-13 00:12:49 +0300

    change seasonView to be 2perRow like_malApp
    Remove WhiteSpace  - Mediaposter
    
    MoeList-3.7.1 make seasonView_2perRow_like_malApp
    
    modify - MoeList-3.7.1  change  seasonView to be  2perRow like_malApp
    
    Update SeasonChartView.kt   for Landscape make it 3 per row.
    
    Update SeasonChartView.kt   for Landscape make it 3 per row.
    
    code refactor +  starCount Box Padding Fix
    
    code refactor  ,
    moving funcs. to related file ,
    starCount Box Padding Fix
    
    Update SeasonChartView.kt  func GetxxxCount readability fix
    
    fix positions  placeholder  vs loadedItems
    
    fix positions  placeholder  vs loadedItems
    
    seasonView_2perRow  reduce Heght to Match Mal.

--- app/src/main/java/com/axiel7/moelist/ui/composables/score/ScoreIndicator.kt
+++ app/src/main/java/com/axiel7/moelist/ui/composables/score/ScoreIndicator.kt
@@ -8,9 +8,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,21 +25,28 @@ fun SmallScoreIndicator(
     score: Float?,
     modifier: Modifier = Modifier,
     fontSize: TextUnit = 14.sp,
-) {
+    textColor: Color = MaterialTheme.colorScheme.outline,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    iconPaddingEnd: Dp = 0.dp,
+    ) {
+
+
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
+            modifier = Modifier.padding(end = iconPaddingEnd),
             painter = painterResource(R.drawable.ic_round_star_16),
             contentDescription = stringResource(R.string.mean_score),
-            tint = MaterialTheme.colorScheme.outline
+            tint = textColor,
         )
         Text(
             text = score.toStringPositiveValueOrUnknown(),
-            modifier = Modifier.padding(horizontal = 4.dp),
-            color = MaterialTheme.colorScheme.outline,
-            fontSize = fontSize
+            modifier = Modifier.padding(horizontal = 4.dp ),
+            color = textColor,
+            fontSize = fontSize,
+            lineHeight = lineHeight,//fontSize*1.2,
         )
     }
 }
