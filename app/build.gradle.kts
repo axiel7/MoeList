9eef1e5a2ba1ef01defa07e0b505b78e50c4663f
Axel <axiel7lopez@gmail.com>
2025-01-16 19:57:45 +0100

    chore(deps): datastore-preferences 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,7 +99,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
 
     implementation("androidx.browser:browser:1.8.0")
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.2")
     implementation("androidx.work:work-runtime:2.10.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
