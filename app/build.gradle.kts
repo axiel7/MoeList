a095699727d7bbab05e53dfed7debcd4b5b2efbe
Axel <axiel7lopez@gmail.com>
2025-01-16 19:58:10 +0100

    chore(deps): activity-compose 1.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,7 @@ dependencies {
     implementation("androidx.compose.material3:material3-android:$materialVersion")
     implementation("androidx.compose.material3:material3-window-size-class:$materialVersion")
 
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
     val lifecycleVersion = "2.8.7"
