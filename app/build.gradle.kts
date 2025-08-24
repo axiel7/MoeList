bcf55d470c8dd9c9fa234c3a5a5ebd7e47016d4d
axiel7 <axiel7lopez@gmail.com>
2025-08-24 09:52:57 +0200

    feat(gradle): disable dependency metadata for APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,6 +97,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    dependenciesInfo {
+        includeInApk = false
+    }
     namespace = "com.axiel7.moelist"
 }
 
