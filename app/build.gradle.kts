20bde09435490dcd019adfdc0fc581a6cc698464
Axel <axiel7lopez@gmail.com>
2025-11-22 20:41:34 +0100

    fix(build): bundle release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,16 @@ android {
     }
     splits {
         abi {
-            isEnable = true
+            // Detect app bundle and conditionally disable split abis
+            // This is needed due to a "Sequence contains more than one matching element" error
+            // present since AGP 8.9.0, for more info see:
+            // https://issuetracker.google.com/issues/402800800
+
+            // AppBundle tasks usually contain "bundle" in their name
+            val isBuildingBundle = gradle.startParameter.taskNames.any { it.lowercase().contains("bundle") }
+
+            // Disable split abis when building appBundle
+            isEnable = !isBuildingBundle
             reset()
             include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
             isUniversalApk = true
